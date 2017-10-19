b7eb55a21f6ae85219e4caf9e0527c027931ecb4
Rhaaheem <rhaaheemjackson@gmail.com>
2017-10-19 14:20:12 -0400

    added new if condition to call method

--- razzy.py
+++ razzy.py
@@ -9,20 +9,21 @@ def hello(bot, update):
     update.message.reply_text(
         'Fuck up {}'.format(update.message.from_user.first_name))
     
-def cosign(bot, update):
+def call(bot, update):
     chat_id = update.message.chat_id
-    text = update.message.text
-    if text == 'Aye Razzy':
+    text = update.message.text.lower()
+    if text == "what's up razzy":
         bot.sendMessage(chat_id, text='Whats good my boy')
+    if text == 'who do you agree with?':
+        bot.sendMessage(chat_id, text='I agree with Rhaaheem, your argument is invalid!')
+    
 
 
 updater = Updater(TOKEN)
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, cosign))
-
-
+updater.dispatcher.add_handler(MessageHandler(Filters.text, call))
 
 updater.start_polling()
 updater.idle()
